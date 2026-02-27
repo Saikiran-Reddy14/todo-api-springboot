dca7ec0bbeb4dbb317eafa09cb8d7aa15804d039
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2026-02-27 23:10:18 +0530

    Add JsonInclude annotation to ApiRespone for null value handling

--- src/main/java/com/example/todos/dto/ApiRespone.java
+++ src/main/java/com/example/todos/dto/ApiRespone.java
@@ -2,9 +2,12 @@ package com.example.todos.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiRespone<T>(
         String message,
         int status,
